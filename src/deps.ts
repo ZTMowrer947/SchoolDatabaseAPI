ca8099d85e4b1e685f23a5a0be4348e875470ec0
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-07-11 14:50:32 -0400

    Add mysql

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,3 @@
+export { Client } from "https://deno.land/x/mysql@v2.9.0/mod.ts";
 export { Application } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+import "https://deno.land/x/dotenv@v2.0.0/load.ts";
