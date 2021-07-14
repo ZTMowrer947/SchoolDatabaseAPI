b968d12e8cd17be6ee7c89f9988158b2d1aedd92
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-07-14 18:14:19 -0400

    Add bcrypt

--- src/deps.ts
+++ src/deps.ts
@@ -7,7 +7,13 @@ export {
   STATUS_TEXT,
 } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 export type {
+  Context,
   RouterContext,
   HttpError,
   Middleware,
 } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+export {
+  hash as bcryptHash,
+  compare as bcryptCompare,
+  genSalt as bcryptGenSalt,
+} from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
